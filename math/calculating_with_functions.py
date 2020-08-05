91dc5929684c64f4ac4189ead5436d04f48bf90a
Heather Nuffer <zzzuzz@gmail.com>
2020-08-05 12:08:47 -0600

    Sets up calculating_with_functions.py but no solution yet

---
+++ math/calculating_with_functions.py
@@ -0,0 +1,49 @@
+"""
+https://www.codewars.com/kata/525f3eda17c7cd9f9e000b39/train/python
+
+This time we want to write calculations using functions and get the results. Let's have a look at some examples:
+
+seven(times(five())) # must return 35
+four(plus(nine())) # must return 13
+eight(minus(three())) # must return 5
+six(divided_by(two())) # must return 3
+
+Looks like the number functions must allow either 0 or 1 parameters. The 1 parameter is an operation function invoked.
+Looks like the operation functions must take a number function invoked as a parameter.
+"""
+
+
+def zero():  # your code here
+
+
+def one():  # your code here
+
+
+def two():  # your code here
+
+
+def three():  # your code here
+
+
+def four():  # your code here
+
+
+def five():  # your code here
+
+
+def six():  # your code here
+
+
+def seven():  # your code here
+
+
+def eight():  # your code here
+
+
+def nine():  # your code here
+
+
+def plus():  # your code here
+def minus():  # your code here
+def times():  # your code here
+def divided_by():  # your code here
