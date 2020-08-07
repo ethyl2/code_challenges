18286f88c379c9ae7a1729c98f06a7bde9d2d21a
Heather Nuffer <zzzuzz@gmail.com>
2020-08-07 11:52:33 -0600

    Adds another solution for invalid_transactions.py

--- invalid_transactions.py
+++ invalid_transactions.py
@@ -41,14 +41,62 @@ class Solution:
                 lookup[name].append(transaction)
                 users_transactions = lookup[name]
                 for users_transaction in users_transactions:
-                    if city != users_transaction[3]:
-                        if abs(int(time) - int(users_transaction[1])) <= 60:
+
+                    users_transaction_list = users_transaction.split(',')
+                    if city != users_transaction_list[3]:
+                        if abs(int(time) - int(users_transaction_list[1])) <= 60:
                             output.add(transaction)
                             output.add(users_transaction)
         return list(output)
 
+    # This version experiments with a nested dictionary.
+    # It excludes looping through elements that have the same name & same city.
+    # But it doesn't seem to be much more efficient.
+    def invalidTransactions2(self, transactions: List[str]) -> List[str]:
+        output = set()
+        lookup = dict()
+
+        for transaction in transactions:
+            name, time, amount, city = transaction.split(',')
+
+            if int(amount) > 1000:
+                output.add(transaction)
+
+            if name not in lookup:
+                lookup[name] = {city: [transaction]}
+            else:
+                if city in lookup[name]:
+                    lookup[name][city].append(transaction)
+                    for current_city, cities_transactions in lookup[name].items():
+                        if current_city != city:
+                            for current_cities_transaction in cities_transactions:
+                                cct_list = current_cities_transaction.split(
+                                    ',')
+                                if abs(int(time) - int(cct_list[1])) <= 60:
+                                    output.add(transaction)
+                                    output.add(current_cities_transaction)
+                else:
+                    for current_city, cities_transactions in lookup[name].items():
+                        for current_cities_transaction in cities_transactions:
+                            cct_list = current_cities_transaction.split(
+                                ',')
+                            if abs(int(time) - int(cct_list[1])) <= 60:
+                                output.add(transaction)
+                                output.add(current_cities_transaction)
+                    lookup[name].update({city: [transaction]})
+
+        return(output)
+
 
 s = Solution()
+
 # ["bob,50,1200,mtv"]
-# print(s.invalidTransactions(["alice,20,800,mtv", "bob,50,1200,mtv"]))
-print(s.invalidTransactions(["alice,20,800,mtv", "alice,50,100,beijing"]))
+# print(s.invalidTransactions2(["alice,20,800,mtv", "bob,50,1200,mtv"]))
+# print(s.invalidTransactions2(
+#    ["alice,20,800,mtv", "bob,50,1200,mtv", "alice,20,700,mtv"]))
+
+# ["alice,20,800,mtv","alice,50,100,beijing"]
+# print(s.invalidTransactions2(["alice,20,800,mtv", "alice,50,100,beijing"]))
+
+# ["alice,50,1200,mtv"]
+# print(s.invalidTransactions2(["alice,20,800,mtv", "alice,50,1200,mtv"]))
