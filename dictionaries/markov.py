3f496e3f6813bf68febe36908364b9c448d5f64d
Heather Nuffer <zzzuzz@gmail.com>
2020-08-12 11:28:44 -0600

    Adds stretch goal of watching number of quotation marks in markov.py

--- dictionaries/markov.py
+++ dictionaries/markov.py
@@ -41,7 +41,8 @@ for i in range(len(words_list)-1):
 
     if words_list[i] not in lookup:
         lookup[words_list[i]] = [words_list[i+1]]
-        if words_list[i][0].isupper() or (words_list[i][0] == '"' and len(words_list[i]) > 1 and words_list[i][1].isupper()):
+        # Check to see if the word should be considered a start or stop
+        if (words_list[i][0].isupper() and words_list[i][-1] != '"') or (words_list[i][0] == '"' and len(words_list[i]) > 1 and words_list[i][1].isupper()):
             starts.add(words_list[i])
         if words_list[i][-1] in '.?!' or (len(words_list[i]) > 1 and words_list[i][-1] == '"' and words_list[i][-2] in '.?!'):
             stops.add(words_list[i])
@@ -49,7 +50,7 @@ for i in range(len(words_list)-1):
         lookup[words_list[i]].append(words_list[i+1])
 
 # Add the last word to starts and stops, if it qualifies for either/both:
-if words_list[i+1][0].isupper() or (words_list[i+1][0] == '"' and len(words_list[i+1]) > 1 and words_list[i+1][1].isupper):
+if (words_list[i+1][0].isupper() and words_list[i][-1] != '"') or (words_list[i+1][0] == '"' and len(words_list[i+1]) > 1 and words_list[i+1][1].isupper):
     starts.add(words_list[i])
 if words_list[i+1][-1] in '.?!' or (len(words_list[i+1]) > 1 and words_list[i+1][-1] == '"' and words_list[i+1][-2] in '.?!'):
     stops.add(words_list[i+1])
@@ -64,11 +65,35 @@ if words_list[i+1][-1] in '.?!' or (len(words_list[i+1]) > 1 and words_list[i+1]
 
 
 for i in range(5):
+    num_opening_quotes = 0
+    num_closing_quotes = 0
+
     first_word = random.choice(list(starts))
+    if first_word[0] == '"':
+        num_opening_quotes += 1
+    if first_word[-1] == '"':
+        num_closing_quotes += 1
     print(first_word, end=' ')
+
     next_word = random.choice(lookup[first_word])
+    if next_word[0] == '"':
+        num_opening_quotes += 1
+    if next_word[-1] == '"':
+        num_closing_quotes += 1
 
     while next_word not in stops:
         print(next_word, end=' ')
         next_word = random.choice(lookup[next_word])
-    print(next_word)
+        if next_word[0] == '"':
+            num_opening_quotes += 1
+        if next_word[-1] == '"':
+            num_closing_quotes += 1
+
+    if num_opening_quotes < num_closing_quotes and next_word[-1] == '"':
+        print(next_word[:-1], end='')
+    else:
+        print(next_word, end='')
+    if num_opening_quotes > num_closing_quotes:
+        print('"')
+
+    print('\n')
