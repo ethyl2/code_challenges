b9b7d0f757646df2265502dc2345116828a7282b
Heather Nuffer <zzzuzz@gmail.com>
2020-08-12 19:27:45 -0600

    Adds another implementation of happy_num.py

--- math/happy_num.py
+++ math/happy_num.py
@@ -10,6 +10,9 @@ Repeat until you get 1, or it loops endlessly in a cycle that doesn't include 1.
 
 class Solution:
 
+    def __init__(self):
+        self.answer_set = set()
+
     def isHappy(self, n: int) -> bool:
         prev = None
         current = n
@@ -28,8 +31,37 @@ class Solution:
             return True
         return False
 
+    def transform(num):
+        # 23
+        pass
+
+    def is_happy_recursive(self, n):
+
+        if n == 1:
+            return True
+        elif n in self.answer_set:
+            return False
+
+        new_num = sum([int(digit)**2 for digit in str(n)])
+        self.answer_set.add(n)
+        return self.is_happy_recursive(new_num)
+
+    def is_happy_recursive2(self, n, answer_set=None):
+        if answer_set == None:
+            answer_set = set()
+        if n == 1:
+            return True
+        elif n in self.answer_set:
+            return False
+
+        new_num = sum([int(digit)**2 for digit in str(n)])
+        self.answer_set.add(n)
+        return self.is_happy_recursive(new_num, answer_set)
+
 
 s = Solution()
 
 # print(s.isHappy(19))
-print(s.isHappy(2))
+# print(s.isHappy(2))
+# print(s.is_happy_recursive(19))
+print(s.is_happy_recursive(2))
