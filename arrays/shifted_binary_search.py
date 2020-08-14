c25fd338d0cb44243da6e82c37dbdea2cd21a545
Heather Nuffer <zzzuzz@gmail.com>
2020-08-14 10:43:56 -0600

    Adds shifted binary search

---
+++ arrays/shifted_binary_search.py
@@ -0,0 +1,57 @@
+"""
+From Sean Chen Lecture 08-14-2020
+
+Given a sorted array that is broken up at some rotation point, and a target, find the target.
+The rotation point is the smallest value.
+
+arr = [3,5,7,9,0,1,2] target = 1  -> 5  Note: The rotation point is the value 0 at index 4.
+
+
+"""
+
+
+def shifted_binary_search(arr, target):
+    rp = find_rotation_point(arr)
+    left = binary_search(arr, target, 0, rp - 1)
+    right = binary_search(arr, target, rp, len(arr) - 1)
+    if left != -1:
+        return left
+    if right != -1:
+        return right
+    return -1
+
+
+def binary_search(arr, target, left, right):
+    if right >= left:
+        mid = left + (right-left) // 2
+    else:
+        return -1
+
+    if arr[mid] == target:
+        return mid
+    elif arr[mid] > target:
+        return binary_search(arr, target, left, mid-1)
+    else:
+        return binary_search(arr, target, mid+1, right)
+
+
+def find_rotation_point(arr):
+    left = 0
+    right = len(arr) - 1
+    while left < right:
+        mid = left + (right - left) // 2
+        if arr[mid] > arr[0]:
+            # If arr[mid] is greater than the first value, the rp is not on the left side
+            left = mid
+        else:
+            # The rp must be somewhere in the right side
+            right = mid
+        # Now, once the floor and the ceiling are adjacent, we've found the rp!
+        if left + 1 == right:
+            return right
+
+
+print(shifted_binary_search([3, 5, 7, 9, 0, 1, 2], 1))
+# print(find_rotation_point([3, 5, 7, 9, 0, 1, 2]))
+# print(binary_search([1, 3, 5, 6], 1, 0, 3))
+# print(shifted_binary_search([1, 2, 3, 4], 3))
---
+++ strings/high_and_low.py
@@ -0,0 +1,11 @@
+"""
+https://www.codewars.com/kata/554b4ac871d6813a03000035
+
+Given a string of space separated numbers, 
+return a string of highest number + space + lowest number
+
+Examples:
+high_and_low("1 2 3 4 5")  # return "5 1"
+high_and_low("1 2 -3 4 5") # return "5 -3"
+high_and_low("1 9 3 4 -5") # return "9 -5"
+"""
