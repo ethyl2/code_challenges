b1715f76a59decee2b3c5bf0c759d6dad5e6f078
Heather Nuffer <zzzuzz@gmail.com>
2020-08-15 19:09:32 -0600

    Adds high_and_low.py

--- strings/high_and_low.py
+++ strings/high_and_low.py
@@ -9,3 +9,23 @@ high_and_low("1 2 3 4 5")  # return "5 1"
 high_and_low("1 2 -3 4 5") # return "5 -3"
 high_and_low("1 9 3 4 -5") # return "9 -5"
 """
+
+# O(n) overall because 3 O(n)s: making the num_list, max(), and min()
+
+
+def high_and_low(s):
+    num_list = [int(num) for num in s.split(' ')]
+    return f"{max(num_list)} {min(num_list)}"
+
+# This second version uses sort, so not as time efficient: O(n log n), but the lookups for the max and min values are O(1)
+# This could be easily modified to use sort() instead of sorted() for more memory efficiency.
+
+
+def high_and_low2(s):
+    num_list = sorted([int(num) for num in s.split(' ')])
+    return f"{num_list[-1]} {num_list[0]}"
+
+
+print(high_and_low2("1 2 3 4 5"))
+print(high_and_low2("1 2 -3 4 5"))
+print(high_and_low2("1 9 3 4 -5"))  # return "9 -5"
