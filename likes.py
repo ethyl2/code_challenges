bae532486f09f0cb9e3782dd7f36457201bd72c2
Heather Nuffer <zzzuzz@gmail.com>
2020-08-18 16:05:08 -0600

    Adds likes.py

---
+++ likes.py
@@ -0,0 +1,45 @@
+"""
+https://www.codewars.com/kata/5266876b8f4bf2da9b000362/train/python
+
+Given an array of strings, where strings are names of people.
+Return a string that is correctly formatted.
+
+Formatting Rules:
+likes [] // must be "no one likes this"
+likes ["Peter"] // must be "Peter likes this"
+likes ["Jacob", "Alex"] // must be "Jacob and Alex like this"
+likes ["Max", "John", "Mark"] // must be "Max, John and Mark like this"
+likes ["Alex", "Jacob", "Mark", "Max"] // must be "Alex, Jacob and 2 others like this"
+"""
+from typing import List
+
+
+def likes(names: List[str]) -> str:
+    # Start with what the strings all have in common
+    output = ' this'
+    # Next, deal with lengths 0 & 1
+    if len(names) <= 1:
+        output = ' likes' + output
+        if len(names) == 0:
+            output = 'no one' + output
+        else:
+            output = names[0] + output
+    # Or, deal with lengths 2 and above
+    else:
+        output = ' like' + output
+        if len(names) == 2:
+            output = f'{names[0]} and {names[1]}' + output
+        elif len(names) == 3:
+            output = f'{names[0]}, {names[1]} and {names[2]}' + output
+        else:
+            output = f'{names[0]}, {names[1]} and {len(names)-2} others' + \
+                output
+    return output
+
+
+print(likes([]))
+print(likes(['Beej']))
+print(likes(['Beej', 'Sean']))
+print(likes(['Beej', 'Sean', 'Kapil']))
+print(likes(['Beej', 'Sean', 'Kapil', 'Andrew']))
+print(likes(['Beej', 'Sean', 'Kapil', 'Andrew', 'Greg']))
