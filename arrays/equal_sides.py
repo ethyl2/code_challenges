d8abe65873b8f0979b38c32be33fd5d21ddcccaf
Heather Nuffer <zzzuzz@gmail.com>
2020-08-20 10:58:15 -0600

    Adds armstrong_number.py

---
+++ arrays/equal_sides.py
@@ -0,0 +1,18 @@
+"""
+https://www.codewars.com/kata/5679aa472b8f57fb8c000047/train/python
+
+Given an array, return the index where the sum of the integers to the left of that index is equal to the sum of the integers
+to the right of that index.
+
+Return -1 if that index does not exist.
+
+Examples:
+[1,2,3,4,3,2,1] -> 3
+[1,100,50,-51,1,1] -> 1
+[20,10,-80, 10, 10 ,15, 35] -> 1
+"""
+from typing import List
+
+
+def find_even_index(arr: List[int]) -> int:
+    pass
