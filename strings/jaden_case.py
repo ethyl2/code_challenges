8719789df83a1edaf33e2d3623b7b395ccdfe92b
Heather Nuffer <zzzuzz@gmail.com>
2020-08-24 15:27:26 -0600

    Adds another implementation of jaden_case.py

--- strings/jaden_case.py
+++ strings/jaden_case.py
@@ -7,10 +7,22 @@ Example:
 Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
 Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
 """
+import string
 
 
 def to_jaden_case(string: str) -> str:
+    """
+    My implementation
+    """
     return ' '.join([word.capitalize() for word in string.split()])
 
 
+def to_jaden_case2(string_input: str) -> str:
+    """
+    Another person's version. string.capwords() is handy.
+    """
+    return string.capwords(string_input)
+
+
 print(to_jaden_case("How can mirrors be real if our eyes aren't real"))
+print(to_jaden_case2("How can mirrors be real if our eyes aren't real"))
