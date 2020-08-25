de256590c017f309ced67a30e784078a2fcbf629
Heather Nuffer <zzzuzz@gmail.com>
2020-08-25 11:01:43 -0600

    Adds iq_test() in parity_outlier.py because it is similar

--- arrays/parity_outlier.py
+++ arrays/parity_outlier.py
@@ -41,3 +41,32 @@ print(find_outlier([160, 3, 1719, 19, 11, 13, -21]))
 
 print(find_outlier2([2, 4, 0, 100, 4, 11, 2602, 36]))
 print(find_outlier2([160, 3, 1719, 19, 11, 13, -21]))
+
+
+'''
+https://www.codewars.com/kata/552c028c030765286c00007d/train/python
+This problem is very similar to above, except that it returns the index + 1, instead of the value.
+Also, the input is a string, not an array
+
+Example: iq_test("2 4 7 8 10") -> 3
+'''
+
+
+def iq_test(numbers: str) -> int:
+    numbers_list = numbers.split()
+    num_first_three_even = 0
+    for i in range(3):
+        if int(numbers_list[i]) % 2 == 0:
+            num_first_three_even += 1
+    isEven = num_first_three_even >= 2
+
+    for i, num in enumerate(numbers_list):
+        if isEven:
+            if int(num) % 2 == 1:
+                return i+1
+        if not isEven:
+            if int(num) % 2 == 0:
+                return i+1
+
+
+print(iq_test("2 4 7 8 10"))
