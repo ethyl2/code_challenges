ab851c4d4e8f76756139c93633fb3d6ec181e55f
Heather Nuffer <zzzuzz@gmail.com>
2020-08-25 21:24:08 -0600

    Adds more efficient approach to smallest_differency.py

--- arrays/smallest_difference.py
+++ arrays/smallest_difference.py
@@ -12,7 +12,7 @@ from typing import List
 
 def find_nums_with_abs_smallest_diff(array_one: List[int], array_two: List[int]) -> List[int]:
     """
-    First approach. Brute-force style. O(n*m) time complexity, O(1) space.
+    First approach. Naive. O(n*m) time complexity, O(1) space.
     """
     output = [None, None]
     current_winner = float('inf')
@@ -26,5 +26,33 @@ def find_nums_with_abs_smallest_diff(array_one: List[int], array_two: List[int])
     return output
 
 
-print(find_nums_with_abs_smallest_diff(
-    [-1, 5, 10, 20, 28, 3], [26, 134, 135, 15, 17]))
+def find_nums_with_abs_smallest_diff2(array_one: List[int], array_two: List[int]) -> List[int]:
+    """
+    Second approach. 
+    Sorts the arrays and then iterates through them. O(n log n + m log m) time.
+    O(1) space.
+    """
+    array_one.sort()
+    array_two.sort()
+    pointer_one = 0
+    pointer_two = 0
+    output = [None, None]
+    current_winner = float('inf')
+    while pointer_one < len(array_one) and pointer_two < len(array_two):
+        abs_diff = abs(array_one[pointer_one] - array_two[pointer_two])
+        if abs_diff < current_winner:
+            current_winner = abs_diff
+            output[0] = array_one[pointer_one]
+            output[1] = array_two[pointer_two]
+        if array_one[pointer_one] < array_two[pointer_two]:
+            pointer_one += 1
+        else:
+            pointer_two += 1
+    return output
+
+
+if __name__ == "__main__":
+    print(find_nums_with_abs_smallest_diff(
+        [-1, 5, 10, 20, 28, 3], [26, 134, 135, 15, 17]))
+    print(find_nums_with_abs_smallest_diff2(
+        [-1, 5, 10, 20, 28, 3], [26, 134, 135, 15, 17]))
